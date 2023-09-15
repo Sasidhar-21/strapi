bed708489255bcc5e755560f1dd41e577aa1e006
Sasidhar-21 <sasidhar2101@gmail.com>
2023-09-15 07:46:58 +0530

    release date added

--- pages/film/[slug].js
+++ pages/film/[slug].js
@@ -70,7 +70,7 @@ const Film = ({ film }) => {
             </h1> 
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter mb-4">
                 <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 py-2">
-                   Plot: {film.attributes.plot}
+                   Release date: {film.attributes.released}
                 </span>                
             </h1> 
                   <h2>Reviews:</h2>
